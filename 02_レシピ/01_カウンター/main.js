bdaacf955312f2201d44b41100385af8c1ffafa4
mitsuo34 <mitsuotajima80@gmail.com>
2022-04-12 15:26:50 +0900

    [便利な機能]

--- 02_レシピ/01_カウンター/main.js
+++ 02_レシピ/01_カウンター/main.js
@@ -1,8 +1,20 @@
 const plus1 = document.getElementById("plus1")
 const number = document.getElementById("number")
+const dobbule = document.getElementById("dobbule")
+const minus1 = document.getElementById("minus1")
 let num = 0
 
 plus1.onclick = function () {
   num += 1
+  number.textContent = num
+}
+
+dobbule.onclick = function () {
+  num *= 2
+  number.textContent = num
+}
+
+minus1.onclick = function () {
+  num -= 1
   number.textContent = num
 }
